f1957c5d30ec760104393fc2d4f5b7d9afedd496
Piyush patil <098piyushpatil@gmail.com>
2025-04-23 07:23:58 +0530

    feat:  added updateProfile , check-username and getUsere endpoint

--- Server/src/controller/user.controller.ts
+++ Server/src/controller/user.controller.ts
@@ -18,18 +18,34 @@ export const getProfile = asyncHandler(async (req: Request, res: Response): Prom
     }
 });
 
-// export const updateProfile = asyncHandler(async (req: Request, res: Response): Promise<any> => {
-//     const { fullname, bio } = req.body;
+export const updateProfileDetails = asyncHandler(async (req: Request, res: Response): Promise<any> => {
     
-//     if(fullname && bio && username ) {
+        const { fullname, bio , username} = req.body;
+        const userId = req.user?._id as string;
 
-//     }
-// });
+        if(!fullname && !bio && !username ) {
+            return res.status(400).json({ message: "Please provide at least one fields to update" });
+        }
 
+        const updateFields: any = {};
+        if (fullname) updateFields.fullname = fullname;
+        if (bio) updateFields.bio = bio;
+        if (username) updateFields.username = username;
+
+        
+        const updatedUser = await User.findByIdAndUpdate(userId, 
+            { $set: updateFields },
+            { new: true, runValidators: true }
+        ).select("-password")
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json({ message: "Profile updated successfully", user: updatedUser });
+});
 
 export const updateProfilePicture = asyncHandler(async (req: Request, res: Response): Promise<any> => {
-
-    try {
         const file = (req.files as any)?.profilePicture[0];
         const userId = req.user?._id;
         
@@ -52,11 +68,35 @@ export const updateProfilePicture = asyncHandler(async (req: Request, res: Respo
         }
 
         return res.status(200).json({ message: "Profile picture updated successfully", user });
+});
 
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ message: "Internal Server Error"});
+export const checkUsernameAvailability = asyncHandler(async (req: Request, res: Response): Promise<any> => {
+    const { username } = req.params;
+
+    if(!username) {
+        return res.status(400).json({ message: "Username is required" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    
+    if(existingUser) {
+        return res.status(200).json({ available: true , message: "Username is already taken" });
+    } else {
+        return res.status(200).json({ available: false , message: "Username is available" });
     }
 });
+
+export const getUserById = asyncHandler(async (req: Request, res: Response): Promise<any> => {
+    const { userId } = req.params;
+
+    const user = await User.findById(userId).select("-password").populate("profilePicture")
+    if (!user){
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ user });    
+});
+
+
 
 
--- Server/src/route/user.routes.ts
+++ Server/src/route/user.routes.ts
@@ -1,16 +1,19 @@
 import express from 'express'
 import { verifyJWT } from '../middleware/auth.middleware'
-import { getProfile, updateProfilePicture } from '../controller/user.controller'
+import { checkUsernameAvailability, getProfile,updateProfileDetails, updateProfilePicture } from '../controller/user.controller'
 import { upload } from "../middleware/multer.middleware"
 const router = express.Router()
 
 router.get("/profile/:username", verifyJWT , getProfile)
-router.post("/img", verifyJWT , upload.fields([
+router.get("/check-username/:username", verifyJWT , checkUsernameAvailability)
+router.get("/getUser/:id" , verifyJWT , getProfile)
+router.put("/updateImg", verifyJWT , upload.fields([
     {
         name: "profilePicture",
         maxCount: 1
     }
 ]) ,updateProfilePicture)
+router.put("/updateProfile", verifyJWT , updateProfileDetails)
 
 
 export default router
